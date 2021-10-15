2c7d4bd2ab6d3d5c7043b1bccb252a6b6849f049
apurbadh <erikduke182@gmail.com>
2021-10-15 22:28:17 +0545

    product component created

---
+++ src/components/Product.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+	export default {
+		props: {
+			name: String
+		}
+	}
+</script>
+<template>
+	<h2>Product</h2>
+	<p>{{ name }}</p>	
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,17 @@
 <script lang="ts">
-	
+	import Product from "../components/Product.vue"	
+
+	export default {
+		data(){
+			return {
+				products : [{name : "test"}, {name : "data"}]
+			}
+		}
+	}
 </script>
 <template>
 	<h1>This is home page</h1>
-	
+	<div v-for="product in products">
+		<Product :name="product.name"/>
+	</div>	
 </template>
